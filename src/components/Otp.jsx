fa1574d94453d73e7d02be305a8945f98d4047d4
ellojuned <ello.juned009@gmail.com>
2023-10-31 15:26:45 +0530

    client phone verification removed

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -12,28 +12,24 @@ const OTPVerification = () => {
   const [isCallConnected, setISCallConnected] = useState(false);
 
   const onCall = async () => {
-    if (phoneNumber.length > 10 || phoneNumber.length < 10) {
-      toast.info("please enter a valid phone number!!");
-    } else {
-      setLoading(true);
+    setLoading(true);
 
-      try {
-        const response = await axios.post(baseUrl, { phoneNumber });
-        if (response.status === 200) {
-          toast.success(response.data.message);
-          setIsValid(true);
-          setISCallConnected(true);
-        }
-        console.log("response", response);
-        setLoading(false);
-      } catch (error) {
-        console.log("error", error);
-        if (error.response) {
-          toast.error(error.response.data.message);
-        }
-        setIsValid(false);
-        setLoading(false);
+    try {
+      const response = await axios.post(baseUrl, { phoneNumber });
+      if (response.status === 200) {
+        toast.success(response.data.message);
+        setIsValid(true);
+        setISCallConnected(true);
       }
+      console.log("response", response);
+      setLoading(false);
+    } catch (error) {
+      console.log("error", error);
+      if (error.response) {
+        toast.error(error.response.data.message);
+      }
+      setIsValid(false);
+      setLoading(false);
     }
   };
 
